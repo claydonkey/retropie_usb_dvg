b8871d1fbfa3b6246bb761a01b93ad802e49d503
Anthony Campbell <anthony@claydonkey.com>
2022-05-20 11:26:30 +0100

    added rpi3 release

--- retropie_setup.sh
+++
@@ -1,3 +0,0 @@
- #!/usr/bin/env bash
- sudo wget https://github.com/claydonkey/mame-libretro/releases/download/v0.1.0-usb_dvg-beta/mame_libretro.so /opt/retropie/libretrocores/lr-mame
- sudo patch /home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-mame.sh lr-mame.patch
---
+++ retropie_setup_rpi4.sh
@@ -0,0 +1,3 @@
+ #!/usr/bin/env bash
+ sudo wget -P /opt/retropie/libretrocores/lr-mame https://github.com/claydonkey/mame-libretro/releases/download/v0.1.0-usb_dvg-beta/mame_libretro.so 
+ sudo patch /home/pi/RetroPie-Setup/scriptmodules/libretrocores/lr-mame.sh lr-mame.patch
